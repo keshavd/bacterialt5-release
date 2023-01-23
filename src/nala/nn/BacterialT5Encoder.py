7fe37c08975c0092cc088b9dcf2716606749d8f2
Keshav Dial <y.inuyasha@gmail.com>
2023-01-23 12:42:14 -0500

    removing token_type_ids

--- src/nala/nn/BacterialT5Encoder.py
+++ src/nala/nn/BacterialT5Encoder.py
@@ -5,6 +5,7 @@ from transformers.data.data_collator import DataCollatorWithPadding
 from .bases.ONNXBase import ONNXBase
 from ..constants import bacterial_t5_encoder_onnx_fh
 from src.nala.encode.tokenizers.DNABertTokenizer import DNABertTokenizer
+from ..structs import BaseModelOutputWithPooling
 
 
 class BacterialT5Encoder(ONNXBase):
@@ -17,6 +18,20 @@ class BacterialT5Encoder(ONNXBase):
         self.tokenizer = DNABertTokenizer()
         self.collate_fn = DataCollatorWithPadding(
             tokenizer=self.tokenizer, padding=True, return_tensors="np"
+        )
+
+    def get_output(self, input_ids, attention_mask, token_type_ids=None):
+        if input_ids.shape[0] > self.max_batch_size:
+            raise ValueError("Input batch size is greater than max batch size")
+        output = self.session.run(
+            None,
+            {
+                "input_ids": input_ids,
+                "attention_mask": attention_mask,
+            },
+        )
+        return BaseModelOutputWithPooling(
+            last_hidden_state=output[0]
         )
 
     def get_base_df_from_tokenized_df(
