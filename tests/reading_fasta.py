f14b97a41493e59de0acc51cec813a9e55854bfc
Keshav Dial <y.inuyasha@gmail.com>
2023-01-23 17:51:38 -0500

    testing file

--- tests/reading_fasta.py
+++ tests/reading_fasta.py
@@ -1,5 +1,5 @@
 from nala.seq.genome_processing import fna_to_df
 from pathlib import Path
 path = Path(".")
-fna_to_df(f"{path.absolute()}/NC_000913.fna")
+fna_to_df(f"{path.absolute()}/vancomycin.fasta")
 
