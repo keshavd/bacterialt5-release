7d89b4ed4666f542a6fb3f69abaaeb1b51d9e2b0
Keshav Dial <y.inuyasha@gmail.com>
2023-01-23 18:01:30 -0500

    FNA Test

--- tests/reading_fasta.py
+++ tests/reading_fasta.py
@@ -1,5 +1,28 @@
-from nala.seq.genome_processing import fna_to_df
-from pathlib import Path
-path = Path(".")
-fna_to_df(f"{path.absolute()}/vancomycin.fasta")
+import unittest
+from parameterized import parameterized
 
+"""
+Test FNA to DF function
+
+* Reads in FNA File
+* Generates DataFrame
+* Returns Length of Nucleotide Sequence
+
+"""
+def get_nucleotide_seq_length(fna_fh):
+    from nala.seq.genome_processing import fna_to_df
+    from pathlib import Path
+
+    path = Path(".")
+    df = fna_to_df(f"{path.absolute()}/{fna_fh}")
+    return df["nucleotide_seq"][0].__len__()
+
+
+class TestReadingFasta(unittest.TestCase):
+    @parameterized.expand(
+        [
+            ("vancomycin.fasta", 65044),
+        ]
+    )
+    def test_reading_fasta(self, fna_fh, expected_length):
+        self.assertEqual(get_nucleotide_seq_length(fna_fh), expected_length)
