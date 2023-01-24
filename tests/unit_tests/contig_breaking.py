a6880e3d05cbe4e2ada1eb577fa6f04761c9eab5
Keshav Dial <y.inuyasha@gmail.com>
2023-01-23 21:46:30 -0500

    refactor

--- tests/unit_tests/contig_breaking.py
+++ tests/unit_tests/contig_breaking.py
@@ -5,14 +5,15 @@ from parameterized import parameterized
 Test breaking contigs into pieces
 """
 
-def get_broken_seqs(fna_fh, piece_count):
+
+def get_broken_seqs(fna_fh, num_breaks):
     from nala.seq.genome_processing import fna_to_df, sequence_break_dist
     import os
 
     path = os.path.dirname(__file__)
     df = fna_to_df(f"{path}/{fna_fh}")
     seq = df["nucleotide_seq"][0]
-    seqs = sequence_break_dist(seq, num_breaks=piece_count)
+    seqs = sequence_break_dist(seq, num_breaks=num_breaks)
     return seqs
 
 
@@ -23,4 +24,4 @@ class TestContigBreaking(unittest.TestCase):
         ]
     )
     def test_reading_fasta(self, fna_fh, piece_count):
-        self.assertEqual(len(get_broken_seqs(fna_fh, piece_count)), piece_count)
+        self.assertEqual(len(get_broken_seqs(fna_fh, piece_count - 1)), piece_count)
